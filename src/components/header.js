aeec7b044a3b25c6ba83e5a7c9e0c815ece65091
DinoWithCurls <adityarajsingh64@gmail.com>
2022-08-11 23:55:53 +0530

    feat: 🎸 Progress Bar done

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = ({page}) => {
     return (
         <div className='flex flex-col justify-center items-center mt-20'>
             <img alt='eden' src={require('../icons/edenlogo.jpg')} />
-            <ProgressBar noOfScreens={page} />
+            <ProgressBar screenNo={page} />
         </div>
     )
 }
